9957939fb0533dcbebaebbadefc9ca0cf3c5758b
Arpita.s <arpita.sahoo637174@gail.com>
2024-01-17 23:16:46 +0530

    app structure committed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { AppInfoComponent } from './app-info/app-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SideNavComponent,
+    AppInfoComponent
   ],
   imports: [
     BrowserModule,
